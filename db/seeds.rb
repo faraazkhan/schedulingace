f2eb2851c771be0d5c9ba081afc3cfebf97f4b3e
Faraaz Khan <faraaz@rationalizeit.us>
2013-04-16 00:28:59 -0400

    adding stripe token to seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,13 +13,13 @@ puts 'ROLES'
   puts 'role: ' << role
 end
 puts 'DEFAULT USERS'
-user = User.find_or_create_by_email :name => 'faraaz khan'.dup, :email => 'faraaz@rationalizeit.us'.dup, :password => 'password'.dup, :password_confirmation => 'password'.dup
+user = User.find_or_create_by_email :name => 'faraaz khan'.dup, :email => 'faraaz@rationalizeit.us'.dup, :password => 'password'.dup, :password_confirmation => 'password'.dup, :stripe_token => 'pk_test_0aJl2L2k98dG9lF3u6zyfDdj'
 puts 'user: ' << user.name
 user.add_role :admin
-user2 = User.find_or_create_by_email :name => 'Silver User', :email => 'user2@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+user2 = User.find_or_create_by_email :name => 'Silver User', :email => 'user2@example.com', :password => 'changeme', :password_confirmation => 'changeme', :stripe_token => 'pk_test_0aJl2L2k98dG9lF3u6zyfDdj'
 user2.add_role :silver
-user3 = User.find_or_create_by_email :name => 'Gold User', :email => 'user3@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+user3 = User.find_or_create_by_email :name => 'Gold User', :email => 'user3@example.com', :password => 'changeme', :password_confirmation => 'changeme', :stripe_token => 'pk_test_0aJl2L2k98dG9lF3u6zyfDdj'
 user3.add_role :gold
-user4 = User.find_or_create_by_email :name => 'Platinum User', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme'
+user4 = User.find_or_create_by_email :name => 'Platinum User', :email => 'user4@example.com', :password => 'changeme', :password_confirmation => 'changeme', :stripe_token => 'pk_test_0aJl2L2k98dG9lF3u6zyfDdj'
 user4.add_role :platinum
 puts "users: #{user2.name}, #{user3.name}, #{user4.name}"
