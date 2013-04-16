a3fa35378d3fe340c07e3c4ec4c14d04aa55d5ca
Faraaz Khan <faraaz@rationalizeit.us>
2013-04-15 23:25:58 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Schedulingace::Application.config.secret_token = '054808f8b5026bcc813765ad8a82a449e1d15790f805ebc716b7e0663b88bd9242a0aee803d4158f9cbcf07cef2594e39373cfca40225a1af431f88ae77f3bb2'
